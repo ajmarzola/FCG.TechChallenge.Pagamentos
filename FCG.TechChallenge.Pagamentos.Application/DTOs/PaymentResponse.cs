dd6379bdadb777efe795f633d7155255be441ea3
Valber Martins <martins_shinigami@hotmail.com>
2025-09-14 18:27:10 -0300

    Reaplica commit correto

---
+++ FCG.TechChallenge.Pagamentos.Application/DTOs/PaymentResponse.cs
@@ -0,0 +1,17 @@
+﻿using FCG.TechChallenge.Pagamentos.Domain.Enums;
+
+namespace FCG.TechChallenge.Pagamentos.Application.DTOs
+{
+    public sealed record PaymentResponse
+        (
+            int Id,
+            string MerchantId,
+            decimal Amount,
+            string Currency,
+            PaymentMethod Method,
+            PaymentStatus Status,
+            DateTime CreatedAtUtc,
+            DateTime? CapturedAtUtc,
+            DateTime? RefundedAtUtc
+        );    
+}
---
+++ FCG.TechChallenge.Pagamentos.Application/Interfaces/IPaymentRepository.cs
@@ -0,0 +1,11 @@
+﻿using FCG.TechChallenge.Pagamentos.Domain.Entities;
+
+namespace FCG.TechChallenge.Pagamentos.Application.Interfaces
+{
+    public interface IPaymentRepository
+    {
+        Task AddAsync(Payment payment, CancellationToken ct = default);
+        Task<Payment?> GetByIdAsync(int id, CancellationToken ct = default);
+        Task SaveChangesAsync(CancellationToken ct = default);
+    }
+}
